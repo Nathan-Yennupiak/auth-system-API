31fcbfbd98bda5aeb28c4cb814ebf28a8f031c99
Nathan-Yennupiak <nathanyennupiak1@gmail.com>
2025-02-03 12:08:07 +0000

    Set up server: Initialize Express app and configure routes, DB connection, and middleware

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import connectToDatabase from './config/database.js';
+import authRoutes from './routes/authRoutes.js';
+import 'dotenv/config';
+
+
+//Initialize express
+const app = express();
+//  Set up the PORT
+const PORT = process.env.PORT || 5000;
+
+// MIDDLEWARES
+// Enable CORS
+app.use(cors());
+// Parse incoming requests with JSON payloads
+app.use(express.json());
+
+//Connect to Database
+connectToDatabase();
+
+// Define routes
+app.use('/api/auth', authRoutes);
+
+//Start server
+app.listen(PORT, () => {
+    console.log(`Server running on PORT : ${PORT}`)
+})
